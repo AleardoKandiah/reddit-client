903115039cc1af353b45a11dbf28e8e23ed6b13d
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-12 20:01:09 +0100

    navigation bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Image from 'next/image'
 import Icon from '../public/Icon.png'
-import { BeakerIcon, ChevronDownIcon, HomeIcon } from '@heroicons/react/solid'
+import { BeakerIcon, ChevronDownIcon, HomeIcon, SearchIcon } from '@heroicons/react/solid'
 import { StarIcon } from '@heroicons/react/outline'
+
+
 
 function Header() {
   return (
@@ -16,11 +18,20 @@ function Header() {
             layout="fill"
             />
         </div>
-            <div className='mx-7 flex items-center xl:min-w-[300px]'>
+         <div className='mx-7 flex items-center xl:min-w-[300px]'>
             <HomeIcon className='h-5 w-5'/>
             <p className='flex-1 ml-2 lg:inline'>Home</p>
             <ChevronDownIcon className='h-5 w-5'/>
-            </div>
+        </div>
+
+    {/* Search */}
+    <form className='flex flex-1 items-center space-x-2 border
+         border-gray-200 rounded-sm bg-gray-100 px-3 py-1'>
+        <SearchIcon className='h-4 w-6 text-gray-400'/>
+        <input className='flex-1 bg-transparent outline-none' type='text' placeholder='Search Reddit'/>
+        <button type='submit' hidden />
+    </form>
+
     </div>
   )
 }

e2b8c70b5d5398d4aac2fa9c7c766934e4cbf96f
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-15 19:47:38 +0100

    header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import Header from '../components/Header'
 
 // wrap whole app with session poviders to utlise hooks
 // Higher order component pattern
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return( 
   <SessionProvider session={session}>
-      <Component {...pageProps} />
+    <div className="h-screen overflow-y-scroll bg-slate-200">
+    < Header />
+
+    <Component {...pageProps} />
+    </div>
   </SessionProvider>
   )
 }

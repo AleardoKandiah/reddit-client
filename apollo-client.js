2a7cecc44b808fe14fc66784d3b89b75f172c1b9
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-16 21:13:58 +0100

    apollo-client

--- apollo-client.js
+++ apollo-client.js
@@ -2,6 +2,9 @@ import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 const client = new ApooloClient({
     url: "https://countries.trevorblades.com",
+    header: {
+        Authorization: `Apikey`
+    }
     cache: new InMemoryCache(),
 });
 

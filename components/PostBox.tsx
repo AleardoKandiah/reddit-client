4e0748517e7bdfef565043df1655248d86bac6f4
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-17 20:19:16 +0100

    post-box style

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -6,12 +6,12 @@ function PostBox() {
 
   return (
     <form>
-        <div>
+        <div className='flex items-center space-x-3'>
            {/* Avatar */}
 
             <input
             disabled={!session}
-            className='bg-gray-50 p-2 pl-5 outline-none'
+            className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
             type='text' placeholder={
                 session ? 'Create a post by entering a title': 'Sign in to post'
             

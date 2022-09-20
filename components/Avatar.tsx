135c34ab03a6f91aa45be27970d3b3cf12d4b303
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-20 20:13:13 +0100

    avatar component size responsiveness

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -14,7 +14,7 @@ function Avatar( {seed, large} : Props) {
 
 
   return (
-    <div className={`relative h-10 w-10 rounded-full border-gray-300 bg-white ${large && 'h-20 w-20'}`}>
+    <div className={`relative overflow-hidden h-10 w-10 rounded-full border-gray-300 bg-white ${large && 'h-20 w-20'}`}>
       <Image 
       layout='fill'
       src={`https://avatars.decebear.com/api/open-peeps/$

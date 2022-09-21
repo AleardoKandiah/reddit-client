c2c174afd01670bc200319d7709094fccca32529
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-21 20:25:39 +0100

    post tiles

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -36,7 +36,7 @@ function PostBox() {
               <div>
                 {/*Body */}
                 <div className='flex items-center px-2 '>
-                  <p className=''>Body</p>
+                  <p className='min-w-[90px]'>Body</p>
                   <input
                   className='m-2 flex-1 bg-blue-50 p-2 outline-none'
                   {...register('postBody')}

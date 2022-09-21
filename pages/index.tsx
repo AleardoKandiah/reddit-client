3d9528f7296e8bf2994b34266acb3a70c2e04bce
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-21 20:13:36 +0100

    Header styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-5xl">
       <Head>
         <title>Reddit Client</title>
         <link rel="icon" href="/favicon.ico" />

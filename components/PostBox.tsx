771c5a0457f70cf9f1afedc7cc3d5c50d5098799
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-23 18:53:59 +0100

    ImageBox photoicon open

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -26,7 +26,7 @@ function PostBox() {
             disabled={!session}
             className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
             type='text' placeholder={
-                session ? 'Create a post by entering a title': 'Sign in to pos'
+                session ? 'Create a post by entering a title': 'Sign in to post'
             
             }/>
 

d03ebc029ee3812eb84b98fe8aa236e06d1746a7
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-09-29 11:47:35 +0100

    Error style

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -74,7 +74,7 @@ function PostBox() {
                 )}
 
 
-                  {/* Error */}
+                  {/* Error*/}
                   {Object.keys(errors).length > 0 && (
                     <div className='space-y-2 p-2 text-red'>
                       {errors.postTitle?.type === 'require' && (
